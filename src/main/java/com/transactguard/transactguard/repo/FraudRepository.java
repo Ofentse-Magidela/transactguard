0109e72545046aa47763e64c4773e74a88884a8f
Ofentse Magidela <magidelaofentse2@gmail.com>
2026-06-20 21:20:48 +0200

    Implement admin endpoints with role-based access control using @PreAuthorize and @EnableMethodSecurity

--- src/main/java/com/transactguard/transactguard/repo/FraudRepository.java
+++ src/main/java/com/transactguard/transactguard/repo/FraudRepository.java
@@ -1,8 +1,10 @@
 package com.transactguard.transactguard.repo;
 
 import com.transactguard.transactguard.entity.FraudFlag;
-import com.transactguard.transactguard.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FraudRepository extends JpaRepository<FraudFlag, Long> {
+    List<FraudFlag> findAllByResolved(boolean resolved);
 }
